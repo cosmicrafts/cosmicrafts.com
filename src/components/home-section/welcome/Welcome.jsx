bb3251a178f5ab9f72bec9fa5f26f1aea5eb4b24
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2024-06-14 02:37:27 -0600

    Launch

--- src/components/home-section/welcome/Welcome.jsx
+++ src/components/home-section/welcome/Welcome.jsx
@@ -1,11 +1,12 @@
-// Welcome.jsx
-
 import React from 'react';
 import './welcome.scss';
 import HomeSection from '../HomeSection';
 import VideoBackground from './VideoBackground'; // Note the uppercase "V"
 import ImageSlider from './ImageSlider'; // Import the ImageSlider component
 import Button from '../../button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWindows, faApple, faLinux } from '@fortawesome/free-brands-svg-icons';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 import 'swiper/css/mousewheel';
 
 const Welcome = (props) => {
@@ -25,11 +26,25 @@ const Welcome = (props) => {
               Play free, battle your way to victory, and collect NFTs & tokens that boost your Metaverse empire.
             </div>
             <div className="btns">
-              <a href="https://play.cosmicrafts.com" target="_blank" rel="noopener noreferrer">
-                <Button className="btn-main">PLAY NOW</Button>
+              <a href="https://cosmicrafts.com/Cosmicrafts.exe" target="_blank" rel="noopener noreferrer">
+                <Button className="btn-second">
+                  <FontAwesomeIcon icon={faWindows} className="icon-margin" /> Windows
+                </Button>
+              </a>
+              <a href="https://cosmicrafts.com/Cosmicrafts.dmg" target="_blank" rel="noopener noreferrer">
+                <Button className="btn-second">
+                  <FontAwesomeIcon icon={faApple} className="icon-margin" /> Mac
+                </Button>
+              </a>
+              <a href="https://cosmicrafts.com/Cosmicrafts.AppImage" target="_blank" rel="noopener noreferrer">
+                <Button className="btn-second">
+                  <FontAwesomeIcon icon={faLinux} className="icon-margin" /> Linux
+                </Button>
               </a>
               <a href="https://cosmicrafts.fandom.com/" target="_blank" rel="noopener noreferrer">
-                <Button className="btn-second">LEARN MORE</Button>
+                <Button className="btn-main">
+                  <FontAwesomeIcon icon={faBook} className="icon-margin" /> Learn More
+                </Button>
               </a>
             </div>
           </div>
