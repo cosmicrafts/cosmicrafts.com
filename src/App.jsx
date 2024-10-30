38292e0be6e803e41795abc9a984347b07debda2
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2024-10-30 17:52:02 -0600

    Privacy Policy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// App component
+// src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'swiper/css';
@@ -7,10 +7,10 @@ import 'swiper/css/effect-fade';
 import 'swiper/css/navigation';
 import './App.scss';
 import PricingSlider from './pages/DAOcalc';
-
 import Header from './components/header/Header';
 import Home from './pages/Home';
-import TeamPage from './pages/TeamPage'; // Corrected import
+import TeamPage from './pages/TeamPage';
+import PrivacyPolicy from './pages/PrivacyPolicy'; // Import the new component
 
 function App() {
   return (
@@ -19,9 +19,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/team" element={<TeamPage />} /> 
-          <Route path="/factions" element={<PricingSlider />} 
-          /> 
+          <Route path="/team" element={<TeamPage />} />
+          <Route path="/factions" element={<PricingSlider />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} /> {/* Add this line */}
         </Routes>
       </div>
     </Router>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -25,22 +25,23 @@ const Header = () => {
     return (
         <div className="header">
             <div className="header__inner">
-            <div className="header__logo">
-    <Link to="/" 
-          state={{ slide: 'welcome' }} 
-          onClick={() => setIsOpen(false)}> {/* Added to close menu  */}
-        <img src={logo} alt="Logo" />
-    </Link>
-</div>
+                <div className="header__logo">
+                    <Link to="/" 
+                          state={{ slide: 'welcome' }} 
+                          onClick={() => setIsOpen(false)}> {/* Added to close menu  */}
+                        <img src={logo} alt="Logo" />
+                    </Link>
+                </div>
                 <div className="burger-menu" onClick={toggleMenu}>
                     {isOpen ? <FaTimes size="30" /> : <FaBars size="30" />}
                 </div>
                 <ul className={`header__nav ${isOpen ? 'nav-active' : ''}`}>
-                <li><Link to="/" state={{ slide: 'welcome' }} onClick={toggleMenu}>Overview</Link></li>
-                <li><Link to="/factions" onClick={toggleMenu}>Factions</Link></li>
-                <li><Link to="/" state={{ slide: 'champion' }} onClick={toggleMenu}>Characters</Link></li>
-                <li><Link to="/team" onClick={toggleMenu}>Team</Link></li>
-            </ul>
+                    <li><Link to="/" state={{ slide: 'welcome' }} onClick={toggleMenu}>Overview</Link></li>
+                    <li><Link to="/factions" onClick={toggleMenu}>Factions</Link></li>
+                    <li><Link to="/" state={{ slide: 'champion' }} onClick={toggleMenu}>Characters</Link></li>
+                    <li><Link to="/team" onClick={toggleMenu}>Team</Link></li>
+                    <li><Link to="/privacy-policy" onClick={toggleMenu}>Privacy Policy</Link></li> {/* Added Privacy Policy link */}
+                </ul>
             </div>
         </div>
     );
