7db8216ea59458c3b5d94c50141a1f9b468682b9
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2025-01-06 00:54:05 -0600

    remove login

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,13 @@
-import { fileURLToPath, URL } from 'url';
-import { defineConfig } from 'vite';
-import environment from 'vite-plugin-environment';
-import vue from '@vitejs/plugin-vue';
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
-dotenv.config({ path: '../../.env' });
-
+// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    emptyOutDir: true,
-    assetsDir: 'assets',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis',
-      },
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:4943',
-        changeOrigin: true,
-      },
-    },
-  },
-  plugins: [
-    vue(),
-    environment('all', { prefix: 'CANISTER_' }),
-    environment('all', { prefix: 'DFX_' }),
-  ],
+  plugins: [vue()],
   resolve: {
-    alias: [
-      { find: 'declarations', replacement: fileURLToPath(new URL('../declarations', import.meta.url)) },
-      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  publicDir: 'src/assets',
-});
+})
